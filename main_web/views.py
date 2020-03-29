62699417b37b506d6267c5047ccfc5b18d6ea77e
xsl <469798015@qq.com>
2020-03-29 16:09:32 +0800

    初步实现前端数据显示

--- main_web/views.py
+++ main_web/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+    exclude_list = []
+            elif field.verbose_name == u"整改措施":
 def index(request):
     return render(request, 'information.html')
